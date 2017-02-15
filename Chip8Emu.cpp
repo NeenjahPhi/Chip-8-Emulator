83ac74e4e4eede30ebe968b0185fd0c5d96fdbdb
Phillip Tran <philtrann@gmail.com>
2017-02-14 23:26:09 -0800

    First Commit

---
+++ Chip8Emu.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Chip8Emu.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+
+int main() {
+	std::cout << "hello" << std::endl;
+
+	return 0;
+}
